5f7f29755985062d7adaea472d70e8f7a8d64109
florentcoen <florent-valery.coen@epfl.ch>
2013-10-14 19:39:21 +0200

    a set of map containing the distances to each goal is now created during the initialization
    the vector<vector<vector<int>>> distancesToGoal is a vector that
    contains maps of integer. Those integers have the value of the distance
    to a specific goal. one map per goal

---
+++ AISokoban/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AISokoban2
+//
+//  Created by Florent-Valéry Coen on 10/6/13.
+//  Copyright (c) 2013 Florent-Valéry Coen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
